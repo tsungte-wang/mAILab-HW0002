aa1eb8acda52c846d9f94f69180bcc7d35a98440
tsungte <tsungte.wang@gmail.com>
2017-04-20 16:40:51 +0800

    add description

--- HW0002.py
+++ HW0002.py
@@ -1,3 +1,6 @@
+#HW0002
+#1. generate five random numbers and print out
+#2. generate 10, 100, 1000, 10000, 100000 random numbers and caculate thier means , standard deviations 
 import numpy as np
 five = np.random.rand(5)
 print("The five random numbers are: ")
